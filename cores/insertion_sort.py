34171264892f4c1c7799b0a39a431bc42e7e3d4d
Jimmy Chang <rocmewtwo@gmail.com>
2024-03-20 11:06:30 +0800

    insertion sort

---
+++ cores/insertion_sort.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Pair:
+    def __init__(self, key: int, value: str):
+        self.key = key
+        self.value = value
+
+    def __lt__(self, other):
+        return self.key < other.key
+
+    def __repr__(self) -> str:
+        return f"({self.key}, {self.value})"
+
+
+def insertion_sort(pairs: List[Pair]):
+    for i in range(len(pairs)):
+        j = i - 1
+        while (j >= 0 and pairs[j + 1] < pairs[j]):
+            pairs[j], pairs[j + 1] = pairs[j + 1], pairs[j]
+            j -= 1
+
+
+pairs = [Pair(3, "cat"), Pair(3, "bird"), Pair(2, "dog")]
+
+print(pairs)
+insertion_sort(pairs)
+print(pairs)
