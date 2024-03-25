2ea22a2978a769db1faa5087cf142a0e2190b772
Jimmy Chang <rocmewtwo@gmail.com>
2024-03-25 11:36:54 +0800

    first bad version

--- leetcode/first_bad_version.py
+++ leetcode/first_bad_version.py
@@ -5,11 +5,11 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         l, r = 1, n
 
-        while l < r:  # stop at l == r: point to the first bad version
+        while l <= r:
             mid = (l + r) // 2
             if isBadVersion(mid):
-                r = mid  # stay r pointer to bad version
+                r = mid - 1
             else:
-                l = mid + 1  # if good version, move left pointer + 1
+                l = mid + 1
 
         return l
