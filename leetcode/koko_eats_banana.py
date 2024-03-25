b7f55853df64a5ff48054358d18f158fbaeef898
Jimmy Chang <rocmewtwo@gmail.com>
2024-03-25 11:40:55 +0800

    koko eats banana

---
+++ leetcode/koko_eats_banana.py
@@ -0,0 +1,21 @@
+import math
+from typing import List
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def can_finish(k):
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p / k)
+
+            return hours <= h
+
+        l, r = 1, max(piles)
+        while l <= r:
+            mid = (l + r) // 2
+            if can_finish(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
