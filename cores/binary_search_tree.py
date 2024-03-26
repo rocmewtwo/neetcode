43f28e922b3a8e8be6ec5f3f9d18cadb2846fa32
Jimmy Chang <rocmewtwo@gmail.com>
2024-03-26 16:41:30 +0800

    inorder

--- cores/binary_search_tree.py
+++ cores/binary_search_tree.py
@@ -16,11 +16,79 @@ def insert(root: Node, val):
     return root
 
 
-def inorrder(root: Node):
-    if root:
-        inorrder(root.left)
-        print(root.val, end=' ')
-        inorrder(root.right)
+def remove(root: Node, val):
+    if not root:
+        return None
+
+    if val > root.val:
+        root.right = remove(root.right, val)
+    elif val < root.val:
+        root.left = remove(root.left, val)
+    else:
+        # case 1: 0 or 1 child
+        # case 2: 2 children
+        if not root.left:
+            return root.right
+        elif not root.right:
+            return root.left
+        else:
+            # find the min node in right subtree
+            min_node = find_min_node(root.right)
+            root.val = min_node.val
+            root.right = remove(root.right, min_node.val)
+
+    return root
+
+
+def inorder(root: Node):
+    if not root:
+        return
+
+    inorder(root.left)
+    print(root.val, end=' ')
+    inorder(root.right)
+
+
+def preorder(root: Node):
+    if not root:
+        return
+
+    print(root.val, end=' ')
+    preorder(root.left)
+    preorder(root.right)
+
+
+def postorder(root: Node):
+    if not root:
+        return
+
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val, end=' ')
+
+
+def reverse_order(root: Node):
+    if not root:
+        return
+
+    reverse_order(root.right)
+    print(root.val, end=' ')
+    reverse_order(root.left)
+
+
+def inorder_list(root: Node) -> list:
+    if not root:
+        return []
+
+    return inorder_list(root.left) + [root.val] + inorder_list(root.right)
+
+
+def find_min_node(root: Node) -> Node:
+    curr = root
+    while curr and curr.left:
+        curr = curr.left
+
+    return curr
 
 
 if __name__ == "__main__":
@@ -32,4 +100,22 @@ if __name__ == "__main__":
     insert(root, 70)
     insert(root, 60)
     insert(root, 80)
-    inorrder(root)
+
+    print(inorder_list(root))
+    min_node = find_min_node(root)
+    print(min_node.val)
+
+    remove(root, 50)
+    print(inorder_list(root))
+
+    inorder(root)
+    print()
+
+    preorder(root)
+    print()
+
+    postorder(root)
+    print()
+
+    reverse_order(root)
+    print()
