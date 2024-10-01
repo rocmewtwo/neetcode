e229f6e5f49fe8999e401168f1bdd78868eb5e9f
Jimmy Chang <rocmewtwo@gmail.com>
2024-10-01 12:07:55 +0800

    853. Car Fleet - Medium

---
+++ neetcode150/2_2_stack/853. Car Fleet.py
@@ -0,0 +1,32 @@
+# 853. Car Fleet - Medium
+# url: https://leetcode.com/problems/car-fleet/
+
+
+from typing import List
+
+
+# time complexity: sorting O(nlogn) + traverse cars O(n) = O(nlogn)
+# space complexity: O(n)
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted(zip(position, speed), reverse=True)
+        stack = []  # store the car fleet time
+
+        for p, s in cars:  # reverse order
+            time = (target - p) / s
+
+            # if the time is greater than the last car in the stack, it will form a new car fleet
+            # otherwise, it will combine with the last car in the stack
+            if not stack or time > stack[-1]:
+                stack.append(time)
+        return len(stack)
+
+
+if __name__ == "__main__":
+    print(Solution().carFleet(target=12, position=[
+          10, 8, 0, 5, 3], speed=[2, 4, 1, 1, 3]))  # 3
+
+    print(Solution().carFleet(target=10, position=[3], speed=[3]))  # 1
+
+    print(Solution().carFleet(target=100, position=[
+          0, 2, 4], speed=[4, 2, 1]))  # 1
